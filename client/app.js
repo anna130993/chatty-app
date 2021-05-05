de48b4575f7c840a92a23991da948392d1b16d32
Anna Podsiadlo <annamagdalena.podsiadlo@gmail.com>
2021-05-05 23:30:13 +0200

    Add first part of the task

---
+++ client/app.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const loginForm = document.querySelector('#welcome-form'),
+      messagesSection = document.querySelector('#messages-section'),
+      messagesList = document.querySelector('#messages-list'),
+      addMessageForm = document.querySelector('#add-messages-form'),
+      userNameInput = document.querySelector('#username'),
+      messageContentInput = document.querySelector('#message-content');
+
+let userName;
+
+loginForm.addEventListener('submit', event => login(event));
+addMessageForm.addEventListener('submit', event => sendMessage(event));
+
+function login(event) {
+    event.preventDefault();
+    if (userNameInput.value) {
+        userName = userNameInput.value;
+        loginForm.classList.remove('show');
+        messagesSection.classList.add('show');
+    } else {
+        window.alert('Type your username first!');
+    }
+};
+
+function sendMessage(event) {
+    event.preventDefault();
+    if (messageContentInput.value) {
+      addMessage(userName, messageContentInput.value);
+      messageContentInput.value = '';
+    } else {
+      window.alert('Type your message first!');
+    }
+  };
+
+function addMessage(author, content) {
+    const message = document.createElement('li');
+    message.classList.add('message');
+    message.classList.add('message--received');
+    if(author === userName) message.classList.add('message--self');
+    message.innerHTML = `
+      <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+      <div class="message__content">
+        ${content}
+      </div>
+    `;
+    messagesList.appendChild(message);
+};
